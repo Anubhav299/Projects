ac6be506c3e2ce450dd04bde54d471719346340d
Anubhav299 <anubhav.linkedin77@gmail.com>
2025-07-10 21:42:15 +0530

    Updated Home and Checkout Page

---
+++ Major Projects/javascript-amazon-project-main/data/cart.js
@@ -0,0 +1,43 @@
+export let cart = [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity : 2
+}, {
+    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity : 1
+}
+];
+
+export function addToCart(productId, quantity)
+{
+  const matchingItem = cart.find(cartItem => cartItem.productId == productId);
+
+  //checking if it already exists then only increasing quantity otherwise adding new to the cart 
+  if (matchingItem)
+  {   
+      matchingItem.quantity += quantity;
+  }
+  else
+  {
+      // cart.push({
+      //     productId: productId,
+      //     quantity: quantity
+      // });
+      cart.push({
+          productId,
+          quantity
+      });
+  }
+}
+
+export function removeFromCart(productId)
+{
+    let newCart = [];
+
+    cart.forEach((cartItem) => {
+        if (cartItem.productId !== productId) {
+            newCart.push(cartItem);
+        }
+    });
+
+    cart = newCart;
+}
