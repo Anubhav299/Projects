d8fa7b7c023ad6041e6d6cd23790fe4eae61b78a
Anubhav299 <anubhav.linkedin77@gmail.com>
2025-07-12 18:04:16 +0530

    save data  and generate HTML for payment summary

--- Major Projects/javascript-amazon-project-main/data/deliveryOptions.js
+++ Major Projects/javascript-amazon-project-main/data/deliveryOptions.js
@@ -11,3 +11,15 @@ export const deliveryOptions = [{
     deliveryDays: 1,
     priceCents: 999
 }];
+
+export function getDelivery(deliveryOptionId)
+{
+    let deliveryOption;
+    
+    deliveryOptions.forEach((option) => {
+        if (option.id === deliveryOptionId) {
+            deliveryOption = option;
+        }
+    });
+    return deliveryOption || deliveryOptions[0];
+}
--- Major Projects/javascript-amazon-project-main/scripts/checkout.js
+++ Major Projects/javascript-amazon-project-main/scripts/checkout.js
@@ -1,3 +1,5 @@
 import { renderOrderSummary } from "../scripts/Checkout/orderSummary.js";
+import { renderPaymentSummary } from "./Checkout/paymentSummary.js";
 
 renderOrderSummary();
+renderPaymentSummary();
