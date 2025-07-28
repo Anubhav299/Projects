09c8afad3611371fbfdfaf5f9dfedbe5d96ee15b
Anubhav299 <anubhav.linkedin77@gmail.com>
2025-07-29 00:58:36 +0530

    create class version of cart

--- Major Projects/javascript-amazon-project-main/data/cart-class.js
+++ Major Projects/javascript-amazon-project-main/data/cart-class.js
@@ -1,69 +1,68 @@
 class Cart{
-    cartItems = undefined;
-    localStorageKey = undefined;
+    cartItems;
+    localStorageKey;
 
-    loadFromStorage()
+    constructor(localStorageKey) {
+        this.localStorageKey = localStorageKey;
+
+        this.loadFromStorage();
+    }
+
+    loadFromStorage()     //this is a shortcut for loadFromStorage: function() which is actually expected
     {
         this.cartItems = JSON.parse(localStorage.getItem(this.localStorageKey));
-
-        if (!this.cartItems)
-        {
+    
+        if (!this.cartItems) {
             this.cartItems = [{
-                        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-                        quantity: 2,
-                        deliveryOptionId: '1'
-                        }, {
-                        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-                        quantity : 1,
-                        deliveryOptionId: '2'
-                    }];
+                productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                quantity: 2,
+                deliveryOptionId: '1'
+            }, {
+                productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+                quantity: 1,
+                deliveryOptionId: '2'
+            }];
         }
     }
 
-    saveToStorage()
-    {
-        localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
+    saveToStorage() {
+            localStorage.setItem(this.localStorageKey, JSON.stringify(this.cartItems));
     }
-
-    addToCart(productId, quantity)
-    {
-    let matchingItem = this.cartItems.find(cartItem => cartItem.productId == productId);
-
-    //checking if it already exists then only increasing quantity otherwise adding new to the cart 
-    if (matchingItem)
-    {   
-        matchingItem.quantity += quantity;
-    }
-    else
-    {
-        // cart.push({
-        //     productId: productId,
-        //     quantity: quantity
-        // });
-        this.cartItems.push({
-            productId,
-            quantity,
-            deliveryOptionId: '1'
-            
-        });
-    }
-        
-        this.saveToStorage();
-    }
-
-    removeFromCart(productId)
-    {
-        let newCart = [];
     
-        this.cartItems.forEach((cartItem) => {
-            if (cartItem.productId !== productId) {
-                newCart.push(cartItem);
+    addToCart(productId, quantity) {
+            let matchingItem = this.cartItems.find(cartItem => cartItem.productId == productId);
+    
+            //checking if it already exists then only increasing quantity otherwise adding new to the cart 
+            if (matchingItem) {
+                matchingItem.quantity += quantity;
             }
-        });
-    
-        this.cartItems = newCart;
-    
-        this.saveToStorage();
+            else {
+                // cart.push({
+                //     productId: productId,
+                //     quantity: quantity
+                // });
+                this.cartItems.push({
+                    productId,
+                    quantity,
+                    deliveryOptionId: '1'
+                
+                });
+            }
+            
+            this.saveToStorage();
+    }
+    removeFromCart(productId) {
+            let newCart = [];
+        
+            this.cartItems.forEach((cartItem) => {
+                if (cartItem.productId !== productId) {
+                    newCart.push(cartItem);
+                }
+            });
+        
+            this.cartItems = newCart;
+        
+            this.saveToStorage();
     }
 
     updateDeliveryOption(productId, deliveryOptionId) {
@@ -77,36 +76,33 @@ class Cart{
     
         this.saveToStorage();
     }
-}
 
+    updateCartQuantity() {
+        //updates the cart quantity @ top right corner
+        let cartQuantity = 0;
+        this.cartItems.forEach((cartItem) => {
+            cartQuantity += cartItem.quantity;
+        })
+        return cartQuantity;
+    }
 
-const cart = {
-    // updateCartQuantity()
-    // {
-    //   //updates the cart quantity @ top right corner
-    //   let cartQuantity = 0;
-    //   this.cartItems.forEach((cartItem) => {
-    //       cartQuantity += cartItem.quantity;
-    //   })
-    //     return cartQuantity;
-    // },
-
-    // updateQuantity(productId, newQuantity)
-    // {
-    //     let matchingItem;
-    //     this.cartItems.forEach((cartItem) => {
-    //         if (cartItem.productId == productId)
-    //             matchingItem = cartItem;
-    //     });
+    updateQuantity(productId, newQuantity) {
+        let matchingItem;
+        this.cartItems.forEach((cartItem) => {
+            if (cartItem.productId == productId)
+                matchingItem = cartItem;
+        });
     
-    //     matchingItem.quantity = newQuantity;
+        matchingItem.quantity = newQuantity;
     
-    //     this.saveToStorage();
-    // }
+        this.saveToStorage();
+    }
 
 }
 
+const cart = new Cart('cart-oop');
+const businessCart = new Cart('cart-business');
 
-// cart.loadFromStorage();
 
-// cart.addToCart('id2');
+console.log(cart);
+console.log(businessCart);
--- Major Projects/javascript-amazon-project-main/scripts/checkout.js
+++ Major Projects/javascript-amazon-project-main/scripts/checkout.js
@@ -2,7 +2,7 @@ import { renderOrderSummary } from "../scripts/Checkout/orderSummary.js";
 import { renderPaymentSummary } from "./Checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./Checkout/checkoutHeader.js";
 
-import '../data/cart-OOP.js';
+import '../data/cart-class.js';
 
 renderCheckoutHeader();
 renderOrderSummary();
