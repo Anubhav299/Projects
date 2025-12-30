2e1372ff408c3f7632cce025576583c7e1d40cc4
Anubhav <anubhav.linkedin77@gmail.com>
2025-12-30 17:14:25 +0530

    update the Product Store App

---
+++ Mini Projects/PERN/Product Store/backend/config/db.js
@@ -0,0 +1,14 @@
+import { neon } from "@neondatabase/serverless";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+
+//creates a SQL connection using our env variables
+const sql = neon(
+  `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require&channel_binding=require`
+);
+//this sql function we export is used as a tagged tempelate literal, which allows us to write SQL Queries safely
+
+export default sql;
--- Mini Projects/PERN/Product Store/backend/server.js
+++ Mini Projects/PERN/Product Store/backend/server.js
@@ -3,21 +3,72 @@ import helmet from "helmet";
 import morgan from "morgan";
 import cors from "cors";
 import dotenv from "dotenv";
-
+import sql from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import { aj } from "./lib/arcjet.js";
+import { error } from "console";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());                    //allows cross origin resource accessing
-app.use(express.json());            //used to parse
-app.use(helmet());                  //security middleware that helps protect the app by setting various HTTP headers
-app.use(morgan("dev"));             //logs the requests
+app.use(cors()); //allows cross origin resource accessing
+app.use(express.json()); //used to parse
+app.use(helmet()); //security middleware that helps protect the app by setting various HTTP headers
+app.use(morgan("dev")); //logs the requests
 
+//applies arcjet rate limiting to all routes
+app.use(async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req, {
+      requested: 1, //specifies that each request consumes one token
+    });
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        res.status(429).json({ error: "Too many Requests, Try again later." });
+      } else if (decision.reason.isBot()) {
+        res.status(403).json({ error: "Bot access denied." });
+      } else {
+        res.status(403).json({ error: "Forbidden." });
+      }
+      return;
+    }
+
+    //check for spoofed bots
+    if ( decision.results.some((result) => result.reason.isBot() && result.reason.isSpoofed())) {
+      res.status(403).json({ error: "Spoof bot detected." });
+      return;
+    }
+
+    next();
+  } catch (error) {
+    console.log("Arcjet Error", error);
+    next(error);
+  }
+});
 
 app.use("/api/products", productRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port : ${PORT}`);
+async function initDB() {
+  try {
+    await sql`
+      CREATE TABLE IF NOT EXISTS products(
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        image VARCHAR(255) NOT NULL,
+        price DECIMAL(10, 2) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+      )
+    `;
+    console.log("Database Initialised Successfully.");
+  } catch (err) {
+    console.log("Error initDB", err);
+  }
+}
+
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port : ${PORT}`);
+  });
 });
