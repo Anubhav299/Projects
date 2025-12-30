af13c9563a103573a9488095ded8b5c241537f2b
Anubhav <anubhav.linkedin77@gmail.com>
2025-12-31 00:25:46 +0530

    made some new

--- Mini Projects/PERN/Product Store/frontend/src/components/NavBar.jsx
+++ Mini Projects/PERN/Product Store/frontend/src/components/NavBar.jsx
@@ -6,7 +6,7 @@ import ThemeSelector from "./ThemeSelector";
 function NavBar() {
   const { pathname } = useResolvedPath();
   const isHomePage = pathname === "/";
-  return (
+  return ( 
     <div className="bg-base-100/80 backdrop-blur-lg border-b border-base-conten/10 sticky top-0 z-50">
       <div className="max-w-7xl mx-auto">
         <div className="navbar px-4 min-h-[4rem] justify-between">
